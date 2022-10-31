b18487ce0d4b0c297589f5e814242495b9416c97
schneidermichael <schneidermichael@outlook.com>
2022-10-31 19:29:17 +0100

    improvement: created components + redesign content

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  //  basePath: '/my-website',
+  basePath: '/my-website',
   images: {
     unoptimized: true
 }
